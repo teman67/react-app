3896c16ac2c44bdafe8654750b5bb0489d15b5dc
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-02-25 10:52:26 +0100

    add my first component to react app

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// function FunctionalGreeting() {
+//   return <h1>Hello, Functional Component!</h1>;
+// }
+
+
+// arrow function
+// const FunctionalGreeting = () => {
+    
+const FunctionalGreeting = () => {
+  return <h1>Hello, Functional Component!</h1>;
+}
+
+export default FunctionalGreeting;
